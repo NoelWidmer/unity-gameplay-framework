d9dd5166cf1d27d0f0401974a265b890fbb38b81
Noël Widmer <noel.widmer@gmail.com>
2017-04-04 20:37:22 +0200

    Initial

---
+++ Assets/GameplayFramework/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Controller
+{
+    private Pawn _pawn;
+
+    public Pawn Pawn
+    {
+        get
+        {
+            return _pawn;
+        }
+    }
+
+
+
+    public event EventHandler PossessedPawn;
+    public event EventHandler UnPossessedPawn;
+
+
+
+    public virtual void Possess(Pawn pawn)
+    {
+        if(pawn == null)
+            throw new ArgumentNullException();
+
+        if(_pawn != null)
+            UnPossess();
+
+        _pawn = pawn;
+
+        EventHandler subscribers = PossessedPawn;
+        if(subscribers != null)
+            subscribers(this, EventArgs.Empty);
+    }
+
+
+
+    public virtual void UnPossess()
+    {
+        if(_pawn == null)
+            return;
+        
+        _pawn = null;
+
+        EventHandler subscribers = UnPossessedPawn;
+        if(subscribers != null)
+            subscribers(this, EventArgs.Empty);
+    }
+}
---
+++ Assets/GameplayFramework/Pawn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn
+{
+    private Controller _controller;
+
+    public Controller Controller
+    {
+        get
+        {
+            return _controller;
+        }
+    }
+
+
+
+    public event EventHandler BecamePossessed;
+    public event EventHandler BecameUnPossessed;
+
+
+
+    public void OnBecamePossessed(Controller controller)
+    {
+        if(controller == null)
+            throw new ArgumentNullException();
+
+        if(_controller != null)
+        {
+            if(_controller.GetHashCode() == controller.GetHashCode())
+                return;
+
+            _controller.UnPossess();
+        }
+
+        _controller = controller;
+
+        EventHandler subscribers = BecamePossessed;
+        if(subscribers != null)
+            subscribers(this, EventArgs.Empty);
+    }
+
+
+    public void OnBecameUnPossessed()
+    {
+        if(_controller == null)
+            return;
+
+        _controller = null;
+
+        EventHandler subscribers = BecameUnPossessed;
+        if(subscribers != null)
+            subscribers(this, EventArgs.Empty);
+    }
+}
