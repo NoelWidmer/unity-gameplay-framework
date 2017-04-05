424bff78bf7e6eba647065c6a56855d48d7bd334
NoelWidmer <noel.widmer@gmail.com>
2017-04-05 20:26:01 +0200

    Added main player classes.

--- Assets/GameplayFramework/PlayerController.cs
+++ Assets/GameplayFramework/PlayerController.cs
@@ -6,5 +6,32 @@ namespace GameplayFramework
 {
     public class PlayerController : Controller
     {
+        private PlayerInput _playerInput;
+        private PlayerCamera _playerCamera;
+        private PlayerHUD _playerHUD;
+
+        public PlayerInput PlayerInput
+        {
+            get
+            {
+                return _playerInput;
+            }
+        }
+
+        public PlayerCamera PlayerCamera
+        {
+            get
+            {
+                return _playerCamera;
+            }
+        }
+
+        public PlayerHUD PlayerHUD
+        {
+            get
+            {
+                return _playerHUD;
+            }
+        }
     }
 }
