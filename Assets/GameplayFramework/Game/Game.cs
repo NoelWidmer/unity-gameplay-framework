c9b561edfaf1bbf6f2d1bef0d3af63c6ed700b85
NoelWidmer <noel.widmer@gmail.com>
2017-04-09 21:50:09 +0200

    Created GameState and GameMode.

--- Assets/GameplayFramework/Game/Game.cs
+++ Assets/GameplayFramework/Game/Game.cs
@@ -1,27 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameplayFramework
 {
     public class Game
     {
-        private Game _instance;
-
-        private Game()
-        {
-        }
-
-        public static void RequestJoin(PlayerController playerController, Action callback)
-        {
-            if(playerController == null)
-                throw new ArgumentNullException();
-
-            if(callback == null)
-                throw new ArgumentNullException();
-
-            //TODO: Handle join
-        }
     }
 }
